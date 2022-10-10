d0046c085716d1214edeae156b67f540c0ec136c
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:42:43 +0500

    Added task 8 in task_8 branch

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+let url = 'https://itstep.org/ua/about';
+function infoAboutURL ( str ) {
+    return `протокол: ${str.match(/\w+(?=:\/\/)/)[0]}, домен: ${str.match(/(?<=\/\/)\w+.\w+(?=\/)/)[0]}, путь: ${str.match(/\/\w+\/.*/)[0]}`
+}
+console.log(infoAboutURL(url));
