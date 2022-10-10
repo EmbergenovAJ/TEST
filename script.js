4fc024b73e4d6a4df7c8697ba1aff65585b63be7
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:43:15 +0500

    Added task 9 in task_9 branch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function separateBy ( str, separator) {
+    let result = [];
+    let a = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== separator) a+=str[i];
+        if (str[i] === separator) {
+            result.push(a);
+            a = '';
+        }
+    }
+    result.push(a);
+    return result;
+}
+console.log(separateBy('10/08/2020', '/'));
