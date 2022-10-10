43ced4ff75712c715f6e1b76fb63602a804fcb7d
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:40:49 +0500

    Added task 5 in task_5 branch

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function abrv( txt ) {
+    txt = txt.split('');
+    let result = txt[0].toUpperCase();
+    for (let i = 0; i < txt.length; i++) {
+        if (txt[i] === ' ' || txt[i] === '-'){
+            result += txt[i+1].toUpperCase();
+        }
+    }
+    return result;
+}
+console.log(abrv('cascading style sheets'));
+console.log(abrv('Объектно-ориентированное программирование'));
