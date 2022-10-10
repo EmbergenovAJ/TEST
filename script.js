afc14b729b590a694454e8f305cc8fc0a22f6fdd
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:41:22 +0500

    Added task 6 in task_6 branch

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function strSummator (str, ...rest) {
+    let result = str;
+    for (let v of rest) {
+        result += v;
+    }
+    return result
+}
+
+console.log(strSummator('a','b','c','d','e','f','g','h','i','j',));
