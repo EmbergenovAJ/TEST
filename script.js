df369eef6a500d89c553ca51560738b39c24a0ae
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:41:56 +0500

    Added task 7 in task_7 branch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function calc (str) {
+    let regexp = /[\-\+\*\/]/;
+    let regexp1 = /(\d)+/g;
+    let a = str.match(regexp1);
+    let zn = str.match(regexp)[0];
+    switch(zn){
+        case '+': return Number(a[0]) + Number(a[1])
+        case '-': return Number(a[0]) - Number(a[1])
+        case '*': return Number(a[0]) * Number(a[1])
+        case '/': return Number(a[0]) / Number(a[1])
+    }
+}
+
+console.log(calc('222 / 2'));
