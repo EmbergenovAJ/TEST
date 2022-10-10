2b54be447a0cbb80067ec4abd3fa9b509b0ed09e
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:40:01 +0500

    Added task 4 in task_4 branch

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+function toCamelCase ( str ) {
+    let a = str.split('');
+    let b = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === '-') {
+            a[i+1] = a[i+1].toUpperCase();
+            a.splice(i,1);
+            i--;
+            continue;
+        }
+        b.push(a[i]);
+    }
+    return b.join('');
+}
+
+console.log(toCamelCase('font-size'));
