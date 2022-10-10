7fb20d93c867a5739fdb298fb58bbabba2a0b6e0
ЕмбергеновА <19ayda967198@gmail.com>
2022-10-10 17:43:46 +0500

    Added task 10 in task_10 branch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function print ( str ) {
+    let result = (arguments[0]).split('');
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i]);
+        if(result[i] === '%') {
+            result[i+1] = arguments[Number(result[i+1])]
+            result.splice(i,1);
+            i--;
+        }
+    }
+    return result.join('');
+}
+
+console.log(print('Today is %1 %2.%3.%4', 'Monday' , 10, 8, 2020));
